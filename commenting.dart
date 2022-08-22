ce09589d72f741762ec0908c8987f95546e0ace7
Ranjit Thakur <ranjitthakur@Ranjits-MacBook-Air.local>
2022-08-22 14:55:03 +0545

    first commit

---
+++ commenting.dart
@@ -0,0 +1,8 @@
+// This is single line commenting
+
+/* this is said to be 
+multi line commenting*/
+
+/// this is also a commenting type
+/// however this is used for class, method, top-level variable
+
---
+++ variable.dart
@@ -0,0 +1,11 @@
+// general way of declarating the variable
+var x =
+    'max'; // this is universal variable type this means that by looking at the value is assign the variable type
+
+void main(List<String> args) {
+  print(x.runtimeType);
+  var y; // remember the default value of var is null
+  print(y.runtimeType);
+}
+
+String me = 'helllo Ranjit'; // this is the declaration string data type
